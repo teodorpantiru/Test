fcccd0b7c4cff4cf8eb55f025dc0afdfd428da6c
Teo <teodorpantiru@yahoo.com>
2025-05-19 21:19:39 +0300

    changes14

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -177,7 +177,7 @@ export default function NavBar({ onNavigate }) {
             Contact
           </a> */}
 
-          <div className="flex gap-4 pt-2">
+          <div className="flex gap-4 pt-2 justify-center items-center">
             <FaBuildingUser />
             <FaTools />
             <FaToolbox />
