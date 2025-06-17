56a34717fd662f726d8291974ef95ef46db91044
Teo <teodorpantiru@yahoo.com>
2025-06-17 23:14:25 +0300

    logofix6

--- app/components/HomePage.jsx
+++ app/components/HomePage.jsx
@@ -19,8 +19,8 @@ export default function HomePage() {
           <div className="flex justify-center lg:justify-start border-b my-5"></div>
           <p className="text-base sm:text-lg md:text-xl text-gray-300 lg:mb-8 sm:mb-10 max-w-2xl mx-auto lg:mx-0 px-2 sm:px-0">
             Cremil T.S.P.I instalează sisteme inteligente de supraveghere,
-            alarme, control acces și detecție incendiu în Cluj, Florești și în
-            toată România. Rapid, profesionist și personalizat.
+            alarme, control acces și detecție incendiu în Cluj, Florești și alte
+            zone din România. Rapid, profesionist și personalizat.
           </p>
         </div>
 
