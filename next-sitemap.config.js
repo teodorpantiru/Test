717d2270bcbd7d5a0a22a8177afb00baa89a5e27
Teo <teodorpantiru@yahoo.com>
2026-02-18 22:28:49 +0200

    Adaptari SEO3

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,6 +2,17 @@
 module.exports = {
     siteUrl: 'https://www.cremil-tspi.com',
     generateRobotsTxt: true,
-    generateIndexSitemap: false, // Generăm un singur fișier sitemap.xml pentru simplitate
+    generateIndexSitemap: false,
     outDir: 'public',
+    additionalPaths: async (config) => [
+        await config.transform(config, '/servicii/montaj-aer-conditionat'),
+        await config.transform(config, '/servicii/proiectare-sisteme-de-securitate'),
+        await config.transform(config, '/servicii/camere-de-supraveghere'),
+        await config.transform(config, '/servicii/automatizari-bariere-porti'),
+        await config.transform(config, '/servicii/sistem-antiefractie'),
+        await config.transform(config, '/servicii/instalare-mentenanta-sisteme-detectie-incendiu'),
+        await config.transform(config, '/servicii/instalare-mentenanta-sistem-videointerfonie'),
+        await config.transform(config, '/servicii/casa-inteligenta'),
+        await config.transform(config, '/servicii/retea-voce-date'),
+    ],
 };
