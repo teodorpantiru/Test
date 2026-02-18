38f7041b48ea142307396dce0b6c85ad57d5221a
Teo <teodorpantiru@yahoo.com>
2026-02-18 21:16:08 +0200

    Adaptari SEO2

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,6 @@
 module.exports = {
     siteUrl: 'https://www.cremil-tspi.com',
     generateRobotsTxt: true,
+    generateIndexSitemap: false, // Generăm un singur fișier sitemap.xml pentru simplitate
+    outDir: 'public',
 };
